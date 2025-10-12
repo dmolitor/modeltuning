277846fce4d2741f1843a64669df978e4d65a443
dmolitor <39706371+dmolitor@users.noreply.github.com>
2025-10-12 18:35:51 +0000

    Deploying to gh-pages from @ dmolitor/modeltuning@deaae307d1c19b0cb8977b5406f1a869124a049a 🚀

---
+++ reference/figures/hex-sticker.R
@@ -0,0 +1,34 @@
+library(hexSticker)
+library(magick)
+
+img <- image_read(here::here("man", "figures", "wizard2.jpg"))
+img <- img |>
+  image_convert("png") |>
+  image_fill(color = "none") |>
+  image_annotate(
+    text = "modeltuning",
+    font = "Brush Script MT",
+    style = "normal",
+    weight = 1000,
+    size = 100,
+    location = "+210+370",
+    color = "gray80"
+)
+
+s <- sticker(
+  filename = here::here("man", "figures", "logo.png"),
+  white_around_sticker = TRUE,
+  img,
+  package = "",
+  s_x = 1,
+  s_y = 1,
+  s_width = 2,
+  s_height = 14,
+  h_size = 3,
+  h_fill = "white",
+  h_color = "#A9A9A9"
+) + ggplot2::theme(plot.margin = ggplot2::margin(b=0, l=0, unit="lines"))
+
+ggplot2::ggsave(plot = s, filename = here::here("man", "figures", "logo.png"))
+
+# Remove the background at remove.bg
